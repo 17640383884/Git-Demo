77c049c4367db827b8699f888ead8cd60193d8d9
17640383884 <xzq17640383884@163.com>
2022-10-06 15:27:22 +0800

    尝试第四次增改

--- demp-mode-1/src/com/xu/MainTest.java
+++ demp-mode-1/src/com/xu/MainTest.java
@@ -3,5 +3,7 @@ package com.xu;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        System.out.println("test change");
     }
 }
